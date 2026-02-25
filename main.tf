819e03324d1a6c47666fce287ea2c7e3f3239989
bkuptanusree-netizen <bkup.tanusree@gmail.com>
2026-02-25 10:32:07 +0530

    Fix formatting and syntax for Terraform resources

--- main.tf
+++ main.tf
@@ -1,99 +1,99 @@
-provider “aws” {
+provider "aws" {
     region = var.region
 }
 # Create VPC
-resource “aws_vpc” “sandbox_vpc” {
-    cidr_block = “10.0.0.0/16”
+resource "aws_vpc" "sandbox_vpc" {
+    cidr_block = "10.0.0.0/16"
 }
 # Subnet
-resource “aws_subnet” “sandbox_subnet” {
-    vpc_id                                     = aws_vpc.sandbox_vpc.id
-    cidr_block                              = “10.0.1.0/24”
+resource "aws_subnet" "sandbox_subnet" {
+    vpc_id                                  = aws_vpc.sandbox_vpc.id
+    cidr_block                              = "10.0.1.0/24"
     map_public_ip_on_launch = true
 }
 # Internet Gateway
-resource “aws_internet_gateway” “igw” {
+resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.sandbox_vpc.id
 }
 
 # Route Table
-resource “aws_route_table” 	“route_table” {
+resource "aws_route_table" 	"route_table" {
     vpc_id = aws_vpc.sandbox_vpc.id
 }
 
     route {
-        cidr_block = “0.0.0.0/0”
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.igw.id
     }
 }
 
-resource “aws_route_table_association” “rta” {
+resource "aws_route_table_association" "rta" {
     subnet_id          = aws_subnet.sandbox_subnet.id
     route_table_id = aws_route_table.route_table.id
 }
 
 # Security Group
-resource “aws_security_group” “sandbox_sg” {
+resource "aws_security_group" "sandbox_sg" {
     vpc_id = aws_vpc.sandbox_vpc.id
 }
 
 ingress {
-   from_port  = 22
-   to_port       = 22
-   protocol      = “tcp”
-   cidr_blocks = [“0.0.0.0/0”]
+   from_port   = 22
+   to_port     = 22
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
 }
 ingress {
-   from_port  = 8080
-   to_port       = 8080
-   protocol      = “tcp”
-   cidr_blocks = [“0.0.0.0/0”]
+   from_port   = 8080
+   to_port     = 8080
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
 }
 egress {
-   from_port  = 0
-   to_port       = 0
-   protocol      = “-1”
-   cidr_blocks = [“0.0.0.0/0”]
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 
 # Create SSH key
-resource “tls_private_key” “ssh_key” {
-    algorithm = “RSA”
+resource "tls_private_key" "ssh_key" {
+    algorithm   = "RSA"
     rsa_bits    = 4096
 }
-resource “aws_key_pair” “generated_key” {
-    key_name = “sandbox-key”
+resource "aws_key_pair" "generated_key" {
+    key_name = "sandbox-key"
     public_key = tls_private_key.ssh_key.public_key_openssh
 }
 # Save private key locally
-resource “local_file” “private_key” {
+resource "local_file" "private_key" {
     content   = tls_private_key.ssh_key.public_key_openssh
-    filename = “sandbox-key.pem” 
+    filename = "sandbox-key.pem" 
 }
 # EC2 Instance
-resource “aws_instance” “sandbox_vm” {
-    ami                                    =  “ami-01794c3812ef4f99a” # Ubuntu (us-east-1)
-    instance_type                  = “t2.micro”
-    subnet_id                         = aws_subnet.sandbox_subnet.id
+resource "aws_instance" "sandbox_vm" {
+    ami                    =  "ami-01794c3812ef4f99a" # Ubuntu (us-east-1)
+    instance_type          = "t2.micro"
+    subnet_id              = aws_subnet.sandbox_subnet.id
     vpc_security_group_ids = [aws_security_group.sandbox_sg.id]
-    key_name                         = aws_key_pair.generated_key.key_name
+    key_name               = aws_key_pair.generated_key.key_name
 
-provisioner “remote-exec” {
+provisioner "remote-exec" {
     inline = [
-       “ sudo apt update -y”,
-       “ sudo apt install python3 -y”
+       " sudo apt update -y",
+       " sudo apt install python3 -y"
 ]
 connection {
-    type              = “ssh”
-    user              = “ubuntu”
+    type              = "ssh"
+    user              = "ubuntu"
     private_key = tls_private_key.ssh_key.private_key_pem
     host              = self.public_ip
   }
 }
-provisioner “local-exec” {
-    command = “ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/inventory.ini ansible/playbook.yml - -private-key sandbox-key.pem -u ubuntu”
+provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/inventory.ini ansible/playbook.yml - -private-key sandbox-key.pem -u ubuntu"
  }
 }
 
