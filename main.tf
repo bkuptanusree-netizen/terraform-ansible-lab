3032242ed21201fe2b41a8e4c479efe938f4fab5
bkuptanusree-netizen <bkup.tanusree@gmail.com>
2026-02-28 10:03:08 +0530

    Update AWS resources and configurations in main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "sandbox_vpc" {
 resource "aws_subnet" "sandbox_subnet" {
     vpc_id                                  = aws_vpc.sandbox_vpc.id
     cidr_block                              = "10.0.1.0/24"
-    map_public_ip_on_launch = true
+    map_public_ip_on_launch                 = true
 }
 # Internet Gateway
 resource "aws_internet_gateway" "igw" {
@@ -18,26 +18,20 @@ resource "aws_internet_gateway" "igw" {
 
 # Route Table
 resource "aws_route_table" 	"public" {
-    vpc_id = aws_vpc.main.id
+    vpc_id = aws_vpc.sandbox_vpc.id
 }
-resource "aws_route" 	"default_route" {
-    route_table_id         = aws_route_table.public.id
-    destination_cidr_block = "0.0.0.0/0"
-    gatewy_id              = aws_internet_gateway.igw.id
-}
-resource "aws_route_table_association" 	"public_assoc" {
-    subnet_id      = aws_subnet.public.id
-    route_table_id = aws_route_table.public.id
-}
-
 resource "aws_route" 	"internet_access" {
     route_table_id         = aws_route_table.public.id
     destination_cidr_block = "0.0.0.0/0"
     gateway_id             = aws_internet_gateway.igw.id
 }
-
+resource "aws_route_table_association" 	"public_assoc" {
+    subnet_id      = aws_subnet.sandbox_subnet.id
+    route_table_id = aws_route_table.public.id
+}
 # Security Group
 resource "aws_security_group" "sandbox_sg" {
+    name = "sandbox-sg"
     vpc_id = aws_vpc.sandbox_vpc.id
 }
 
@@ -73,31 +67,33 @@ resource "aws_key_pair" "generated_key" {
 }
 # Save private key locally
 resource "local_file" "private_key" {
-    content   = tls_private_key.ssh_key.public_key_openssh
+    content   = tls_private_key.ssh_key.public_key_pem
     filename = "sandbox-key.pem" 
 }
 # EC2 Instance
 resource "aws_instance" "sandbox_vm" {
-    ami                    =  "ami-01794c3812ef4f99a" # Ubuntu (us-east-1)
-    instance_type          = "t2.micro"
-    subnet_id              = aws_subnet.sandbox_subnet.id
-    vpc_security_group_ids = [aws_security_group.sandbox_sg.id]
-    key_name               = aws_key_pair.generated_key.key_name
+    ami                         =  "ami-01794c3812ef4f99a" # Debian (us-east-1)
+    instance_type               = "t2.micro"
+    subnet_id                   = aws_subnet.sandbox_subnet.id
+    vpc_security_group_ids      = [aws_security_group.sandbox_sg.id]
+    key_name                    = aws_key_pair.generated_key.key_name
+    associate_public_ip_address = true
 
+connection {
+    type              = "ssh"
+    user              = "admin"
+    private_key = tls_private_key.ssh_key.private_key_pem
+    host              = self.public_ip
+  }
 provisioner "remote-exec" {
     inline = [
        " sudo apt update -y",
        " sudo apt install python3 -y"
 ]
-connection {
-    type              = "ssh"
-    user              = "ubuntu"
-    private_key = tls_private_key.ssh_key.private_key_pem
-    host              = self.public_ip
-  }
+
 }
 provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/inventory.ini ansible/playbook.yml - -private-key sandbox-key.pem -u ubuntu"
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i ansible/inventory.ini ansible/playbook.yml - -private-key sandbox-key.pem -u admin"
  }
 }
 
